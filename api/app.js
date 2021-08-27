df9ca679715c87640b003b1811bbc2c88256f57f
reveljean <jbaptiste.revel@reseau.eseo.fr>
2021-08-27 10:13:26 +0200

    Local API and client connect

--- api/app.js
+++ api/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require('cors');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+controller_API_Router = require('./routes/controller_API');
 
 var app = express();
 
@@ -15,12 +17,14 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Menu', controller_API_Router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
