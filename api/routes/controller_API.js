9751cca97b76f321179f47b5b5913e41bbea6c2c
reveljean <jbaptiste.revel@reseau.eseo.fr>
2021-08-31 17:30:39 +0200

    client change for useState used / mysql db create / insert items ok

--- api/routes/controller_API.js
+++ api/routes/controller_API.js
@@ -1,8 +1,52 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
-router.get("/", function (req, res, next) {
-	res.send("API is working !!!");
+const db = mysql.createPool({
+	host: "localhost",
+	user: "root",
+	password: "root",
+	database: "crud_db"
+});
+
+
+router.get("/get", function (req, res) {
+	const get_sql = "SELECT * from items";
+	db.query(get_sql, (err, result) => {
+		res.send(result);
+		console.log("Items is correctly get");
+	
+	});
+
+
+});
+
+
+router.get("/", function (req, res) {
+	res.send("Items is correctly add");
+});
+
+
+
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.post("/insert", function (req, res) {
+	const label = req.body.label;
+	const description = req.body.description;
+	const price = req.body.price;
+	const category = req.body.category;
+	const image = req.body.image;
+
+	
+	const insert_sql = "INSERT INTO items (label, description, price, category, image) VALUES (?, ?, ?, ?, ?)";
+	db.query(insert_sql, [label, description, price, category, image], (err, result) => {
+		console.log("Items is correctly add");
+	//	res.render("Items is correctly add");
+	});
+
+
+	//insert into label values ??
 })
 
-module.exports = router;
+module.exports = router; 
--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-import * as React from 'react';
-import { render } from '@testing-library/react';
+//import * as React from 'react';
+import React, { useState } from 'react';
+import Axios from "axios";
 
-class App extends React.Component {
-    constructor(props) {
+function App() {
+
+
+    const [label, setLabel] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [image, setImage] = useState('');
+
+   /* constructor(props) {
         super(props);
         this.state = { apiResponse: "" };
     }
-
 
     callAPI() {
         fetch("http://localhost:9000/Menu")
@@ -18,22 +25,56 @@ class App extends React.Component {
 
     componentWillMount() {
         this.callAPI();
-    }
+    }*/
 
+    
+    const submitReview = () => {
+        Axios.post("http://localhost:9000/Menu/insert",
+            {
+                label: label,
+                description: description,
+                price: price,
+                category: category,
+                image: image 
+            }).then(() => {
+                alert("sucessfull insert");
+            });
+    };
 
-    render() {
         return (
             <div className="App">
-              <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-               
-                  YOUHOU
-            
-                </header>
-                <p>{this.state.apiResponse}</p>
-            </div>
-             );
-     }
-}
 
+                <h1>Web application</h1>
+
+                <div className="form">
+                    <label>Label</label>
+                    <input type="text" name="label" onChange = {(e) => {
+                        setLabel(e.target.value);
+                    }} />
+                    <label>Description</label>
+                    <input type="text" name="description" onChange={(e) => {
+                        setDescription(e.target.value);
+                    }} />
+                    <label>Price</label>
+                    <input type="text" name="price" onChange={(e) => {
+                        setPrice(e.target.value);
+                    }}/>
+                    <label>Category</label>
+                    <input type="text" name="category" onChange={(e) => {
+                        setCategory(e.target.value);
+                    }}/>
+                    <label>Image</label>
+                    <input type="text" name="image" onChange={(e) => {
+                        setImage(e.target.value);
+                    }}/>
+
+                    <button onClick={submitReview}>Submit</button>
+                </div>
+
+             
+             </div>
+            );
+     
+}
+    
 export default App;
