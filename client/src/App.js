8df8a1d52195610a6ee32d021ada7db504f7628f
reveljean <jbaptiste.revel@reseau.eseo.fr>
2021-08-31 17:51:42 +0200

    client get items ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,16 @@
 import './App.css';
 //import * as React from 'react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from "axios";
 
 function App() {
-
 
     const [label, setLabel] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
     const [category, setCategory] = useState('');
     const [image, setImage] = useState('');
-
-   /* constructor(props) {
-        super(props);
-        this.state = { apiResponse: "" };
-    }
-
-    callAPI() {
-        fetch("http://localhost:9000/Menu")
-            .then(res => res.text())
-            .then(res => this.setState({ apiResponse: res }));
-    }
-
-    componentWillMount() {
-        this.callAPI();
-    }*/
-
+    const [list, setList] = useState([]);
     
     const submitReview = () => {
         Axios.post("http://localhost:9000/Menu/insert",
@@ -41,39 +25,49 @@ function App() {
             });
     };
 
-        return (
-            <div className="App">
+    useEffect(() => {
+        Axios.get("http://localhost:9000/Menu/get").then((response) => {
+            setList(response.data);
+        });
+    }); 
 
-                <h1>Web application</h1>
+    return (
+        <div className="App">
 
-                <div className="form">
-                    <label>Label</label>
-                    <input type="text" name="label" onChange = {(e) => {
-                        setLabel(e.target.value);
-                    }} />
-                    <label>Description</label>
-                    <input type="text" name="description" onChange={(e) => {
-                        setDescription(e.target.value);
-                    }} />
-                    <label>Price</label>
-                    <input type="text" name="price" onChange={(e) => {
-                        setPrice(e.target.value);
-                    }}/>
-                    <label>Category</label>
-                    <input type="text" name="category" onChange={(e) => {
-                        setCategory(e.target.value);
-                    }}/>
-                    <label>Image</label>
-                    <input type="text" name="image" onChange={(e) => {
-                        setImage(e.target.value);
-                    }}/>
+            <h1>Web application</h1>
 
-                    <button onClick={submitReview}>Submit</button>
-                </div>
+            <div className="form">
+                <label>Label</label>
+                <input type="text" name="label" onChange = {(e) => {
+                    setLabel(e.target.value);
+                }} />
+                <label>Description</label>
+                <input type="text" name="description" onChange={(e) => {
+                    setDescription(e.target.value);
+                }} />
+                <label>Price</label>
+                <input type="text" name="price" onChange={(e) => {
+                    setPrice(e.target.value);
+                }}/>
+                <label>Category</label>
+                <input type="text" name="category" onChange={(e) => {
+                    setCategory(e.target.value);
+                }}/>
+                <label>Image</label>
+                <input type="text" name="image" onChange={(e) => {
+                    setImage(e.target.value);
+                }}/>
 
+                <button onClick={submitReview}>Submit</button>
+            </div>
+
+            {list.map((val) => {
+                return <h1>{val.label} {val.category} {val.price}</h1>
+                })
+            }
              
-             </div>
-            );
+        </div>
+    );
      
 }
     
